73fa7ba85ca41d2f98d64c9899dcc36cd4b08074
kevin Pechersky <kev.pechersky@gmail.com>
2022-12-02 00:19:09 -0500

    updatedsignin
    hi

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useUserAuth } from '../context/UserAuthContext'
 import { Button } from "react-bootstrap";
 import { Navigate } from 'react-router-dom';
-
+import Form from 'react-bootstrap/Form';
 const Account = () => {
 
 
@@ -20,13 +20,59 @@ const Account = () => {
   return (
     <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 px-md-5' style={{marginTop: '200px'}}>
       <div className='justify-content-center align-items'>
-          Email Address: {user && user.email}
+      <Form>
+     
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label><b>Email Address: </b>{user && user.email} </Form.Label>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label><b>Name: </b>{user && user.fname} , {user &&user.lname}  
+    </Form.Label> </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCard">
+        <Form.Label><b >Cardinfo: {user&& user.card} </b></Form.Label>
+        <Form.Control type="new_card" placeholder= "enter new card number" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formOldPassword">
+        <Form.Label><b>Old Password: {user && user.password} </b></Form.Label>
+        <Form.Control type="password" placeholder="Confirm old password" />
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formNewPassword">
+        <Form.Label><b>New Password:  </b></Form.Label>
+        <Form.Control type="new_password" placeholder= "change password" />
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formBasicPasswordChange">
+        <Form.Label><b>Confirm Password Change: </b></Form.Label>
+        <Form.Control type="new_password" placeholder="re-enter new pass-word" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formDOB">
+        <Form.Label><b >DOB: {user&& user.dob} </b></Form.Label>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formphonenumber">
+        <Form.Label><b > Phonenumber: {user&& user.phone} </b></Form.Label>
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Confirm Changes" />
+      </Form.Group>
+
+      
+      <Button variant="danger" type="submit">
+        Submit changes
+            
+      </Button>
+      <Button variant="dark" type="deposit/withdraw">
+        Deposit/Withdraw
+            
+      </Button>
+    </Form> 
+        
       </div>
-      <div className="d-grid gap-2">
-            <button onClick={handleLogout} className="btn btn-primary" variant="primary">
-              Log Out
-            </button>
-          </div>
+           
     </div>
   )
 }
