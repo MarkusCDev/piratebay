10768850f51a00ab2943887953af8428aa4c89ad
kevin Pechersky <kev.pechersky@gmail.com>
2022-12-03 19:15:39 -0500

    Button to move down footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../App.css'
 
 export const Footer = () => {
   return (
-    <footer className="bg-dark" style={{position: 'absolute', left: '0', bottom: '0', right:'0'}}>
+    <footer className="bg-dark" style={{left: '0', bottom: '0', right:'0'}}>
         <div className="container d-flex justify-content-center">
           <Link to="/secret"><span className="text-muted">PirateBay Inc. 2022</span></Link>
         </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div> 
+      <div style={{marginTop: "60px"}}>Cart</div>
+      <div><button type="button" className="btn btn-light"
+    style= {{marginTop: "1000px"}} > </button></div>
+   </div>
   )
 }
 
