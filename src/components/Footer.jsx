701c11cf2cc550a4730ed1f118a127d624b13d90
MarkusCDev <markus.cc01@gmail.com>
2022-12-08 12:19:07 -0500

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../App.css'
 
 export const Footer = () => {
   return (
-    <footer className="py-5 w-100 bg-dark" style = {{bottom: '0', right: '0', left: '0'}}>
+    <footer  className="py-5 w-100 bg-dark" style = {{bottom: '0', right: '0', left: '0'}}>
         <div className="container d-flex justify-content-center">
           <Link className="text-decoration-none" to="/secret"><span className="text-muted">PirateBay Inc. 2022</span></Link>
         </div>
